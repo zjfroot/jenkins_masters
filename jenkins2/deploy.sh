8b6a649cdd49bfc74685b0be689e1344340d2a32
Jifeng Zhang <zjfroot@gmail.com>
2016-05-27 07:58:42 +0000

    added xvfb buffer and deploy script

--- jenkins2/deploy.sh
+++ jenkins2/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -x
+echo rebuild container
+./build.sh
+echo stopping old container
 docker stop jenkins2
+echo removing old container
 docker rm jenkins2
+echo start new container
 docker run -d -p 8080:8080 -p 50000:50000 --name jenkins2 -v /home/ec2-user/jenkins_homes/jenkins2_home/:/var/jenkins_home jenkins2
